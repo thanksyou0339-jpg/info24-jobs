337aa4f6bb73452d5d4cef1d0c13b792f54503b1
thanksyou0339-jpg <thanksyou0339@gmail.com>
2026-09-04 17:45:30 +0500

    Create firebase-config.js with configuration template
    Added a new Firebase configuration file with a template for setup.

---
+++ firebase-config.js
@@ -0,0 +1,18 @@
+const INFO24_FIREBASE_CONFIG = null;
+
+/*
+    INFO24 JOBS کے لیے نیا Firebase project بننے کے بعد
+    یہاں نئی Firebase configuration رکھی جائے گی۔
+
+    مثال:
+
+    const INFO24_FIREBASE_CONFIG = {
+        apiKey: "...",
+        authDomain: "...",
+        projectId: "...",
+        storageBucket: "...",
+        messagingSenderId: "...",
+        appId: "..."
+    };
+
+*/
